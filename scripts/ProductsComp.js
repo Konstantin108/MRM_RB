4ddbedf3f48d0ccb12082f002d7d6b0493a5427c
Kostya <Kpstya@mail.ru>
2020-07-25 20:27:16 +0300

    added json, methods do not work

---
+++ scripts/ProductsComp.js
@@ -0,0 +1,47 @@
+Vue.component('products', {
+    data() {
+        return {
+            catalogUrl: '/productsData.json',
+            products: []
+        }
+    },
+    mounted() {
+        this.$parent.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    },
+    template: `
+        <div class="container">
+            <product v-for="product of products"
+                     :key="product.id_product"
+                     :product="product">
+            </product>
+        </div>
+    `
+});
+
+Vue.component('product', {
+    props: ['product'],
+    template: `
+         <div class="block-for-product">
+              <div class="name">{{product.name}}</div>
+                   <div class="product-content">
+                        <div class="block-for-title">
+                            <div class="num">{{product.col}}</div>
+                            <div class="price">{{product.price}}</div>
+                        </div>
+                        <div class="block-for-btn">
+                            <button @click="decreaseCol(product)">
+                                <span class="text-btn">-</span>
+                            </button>
+                            <button @click="increaseCol(product)">
+                                <span class="text-btn">+</span>
+                            </button>
+                   </div>
+              </div>
+         </div>
+    `
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,30 @@
+
+
 const app = new Vue({
     el: '#app',
     data: {
-        products: [
-            {name: 'АП ЖКХ', price: 0.13, total: 0, col: 0, fixTotal: 0},
-            {name: 'АП ГИБДД', price: 0.11, total: 0, col: 0, fixTotal: 0},
-            {name: 'БКИ', price: 0.30, total: 0, col: 0, fixTotal: 0},
-            {name: 'УК (МБ)', price: 0.04, total: 0, col: 0, fixTotal: 0},
-            {name: 'МП', price: 0.24, total: 0, col: 0, fixTotal: 0},
-            {name: 'МП реактивация', price: 0.10, total: 0, col: 0, fixTotal: 0},
-            {name: 'Клиенты', price: 0.00, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбермобайл', price: 0.12, total: 0, col: 0, fixTotal: 0},
-            {name: 'Актуализация', price: 0.02, total: 0, col: 0, fixTotal: 0},
-            {name: 'Перевод пенсии', price: 0.25, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбор согласий', price: 0.03, total: 0, col: 0, fixTotal: 0},
-            {name: 'PUSH-уведомления', price: 0.04, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбермобайл', price: 0.24, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбермобайл', price: 0.30, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбермобайл', price: 0.36, total: 0, col: 0, fixTotal: 0},
-            {name: 'КК', price: 0.99, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбор "Лицо"', price: 0.06, total: 0, col: 0, fixTotal: 0},
-            {name: 'Сбор "Голос"', price: 0.06, total: 0, col: 0, fixTotal: 0},
-            {name: 'ОККО', price: 0.1, total: 0, col: 0, fixTotal: 0},
-            {name: 'СберМаркет', price: 0.04, total: 0, col: 0, fixTotal: 0},
-        ],
+        // products: [
+        //     {name: 'АП ЖКХ', price: 0.13, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'АП ГИБДД', price: 0.11, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'БКИ', price: 0.30, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'УК (МБ)', price: 0.04, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'МП', price: 0.24, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'МП реактивация', price: 0.10, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Клиенты', price: 0.00, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбермобайл', price: 0.12, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Актуализация', price: 0.02, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Перевод пенсии', price: 0.25, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбор согласий', price: 0.03, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'PUSH-уведомления', price: 0.04, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбермобайл', price: 0.24, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбермобайл', price: 0.30, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбермобайл', price: 0.36, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'КК', price: 0.99, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбор "Лицо"', price: 0.06, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'Сбор "Голос"', price: 0.06, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'ОККО', price: 0.1, total: 0, col: 0, fixTotal: 0},
+        //     {name: 'СберМаркет', price: 0.04, total: 0, col: 0, fixTotal: 0},
+        // ],
         totalSum: 0,
         norma: '',
         price: '',
@@ -47,16 +49,23 @@ const app = new Vue({
                 item.total = 0;
                 item.col = 0;
             }
+        },
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    alert('Нет связи с сервером');
+                });
         }
     },
     computed: {
         fixedTotalSum() {
             return this.totalSum.toFixed(2);
         },
-        income(){
+        income() {
             return this.fixedTotalSum * this.price;
         },
-        fixedIncome(){
+        fixedIncome() {
             return this.income.toFixed(1);
         }
     },
