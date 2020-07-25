de52f471e4e4114d01746593be014e4c9467def9
Kostya <Kpstya@mail.ru>
2020-07-25 23:21:23 +0300

    change v

--- scripts/SignatureComp.js
+++ scripts/SignatureComp.js
@@ -1,12 +1,16 @@
 Vue.component('signatureComp', {
     template: `
-        <div class="signature">
-        <div class="clr-signature"></div>
-            <div class="my-signature">
-                <span class="v-info"><i>Version 2.7</i></span>
-                <span class="dev-info">developed by Kpstya</span>
+        <transition 
+            appear
+            name="slide-in-fw">
+            <div class="signature">
+            <div class="clr-signature"></div>
+                <div class="my-signature">
+                    <span class="v-info"><i>Version 2.9</i></span>
+                    <span class="dev-info">developed by Kpstya</span>
+                </div>
             </div>
-        </div>
+        <transition>   
     `,
     mounted() {
         console.log(this);
